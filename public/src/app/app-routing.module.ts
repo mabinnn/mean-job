4a95c0c28b138fcbf7b7c91a28e1fa0c532e2bc9
Marvin Alganes <marvin.alganes@gmail.com>
2017-06-26 16:36:37 -0700

    Added login and registration

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { NewentryComponent } from './newentry/newentry.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: "", pathMatch: 'full', component: LoginComponent },
+  {path: "register", component:  RegistrationComponent },
+  {path: "homepage", component: NewentryComponent},
+  {path: "dashboard", component: DashboardComponent}
 ];
 
 @NgModule({
